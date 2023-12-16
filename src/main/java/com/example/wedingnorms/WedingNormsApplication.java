3729c0e25954d0fe16d18599d30ae1a3b17816e9
Stellareign <tuguran@mail.ru>
2023-12-16 20:15:54 +0300

    start

---
+++ src/main/java/com/example/wedingnorms/WedingNormsApplication.java
@@ -0,0 +1,13 @@
+package com.example.wedingnorms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedingNormsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WedingNormsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wedingnorms/WedingNormsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wedingnorms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedingNormsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
